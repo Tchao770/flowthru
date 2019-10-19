3c843e3ac2c723e1f5d14e95e4d04c6709e88cf2
janeen Yamak <Janeen_Yamak@janeen.local>
2019-10-18 18:32:30 -0700

    Auth0 Frontend Integration

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+// src/App.js
+
+import React from "react";
+import NavBar from "./components/NavBar";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import PrivateRoute from "./components/PrivateRoute";
+
+function App() {
+  return (
+    <div className="App">
+      {/* New - use BrowserRouter to provide access to /profile */}
+      <BrowserRouter>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
