ef48112e70d035c13a9cd584ea23ab23ea19d538
Tom <tchao770@gmail.com>
2021-02-24 03:52:26 -0800

    Bugfix to AvatarMenu.js, separated profile link

--- web-app/src/components/AvatarMenu.js
+++ web-app/src/components/AvatarMenu.js
@@ -3,20 +3,22 @@ import React from 'react'
 import { Dropdown, Menu, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 
-
-const MenuItemExtracted = props => {
-    return <Menu {...props}>{props.children} </Menu>
+// Must have a separate component in order to avoid errors regarding Menu.hover properties
+function ProfileLink(props) {
+    return (
+        <Link to="/profile">
+            <Menu.Item {...props} key="profile">Profile</Menu.Item>
+        </Link>
+    );
 }
 
 function PageMenu(props) {
     return (
         <Menu {...props}>
-            <Menu.ItemGroup  title={props.username}>
-                <Link to="/profile">
-                    <Menu.Item  key="profile">Profile</Menu.Item>
-                </Link>
-                <Menu.Item  key="events">User Settings</Menu.Item>
-                <Menu.Item  key="logout">Log Out</Menu.Item>
+            <Menu.ItemGroup title={props.username}>
+                <ProfileLink />
+                <Menu.Item key="events">User Settings</Menu.Item>
+                <Menu.Item key="logout">Log Out</Menu.Item>
             </Menu.ItemGroup>
         </Menu>
     );
